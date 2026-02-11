e2e95ecb524001462ff7110aed6ad3f97c8ce56a
Gus <wischma@wwu.edu>
2026-02-10 18:32:28 -0800

    seperated Game manager into Playermanager and GameManager

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,9 +1,17 @@
+import java.util.ArrayList;
+
 public class GameManager {
 
-    private static GameManager _instance;
 
     // Members
+    private static GameManager _instance;
+    private Player _currentPlayer;
+    private int _currentDay;
+    private GameBoard _gameBoard;
     private TurnAction _playerAction = new Upgrade();
+    private RulesPackage _rules;
+
+
 
     // Constructors
     private GameManager() {}
@@ -17,11 +25,230 @@ public class GameManager {
     }
 
     // Methods
+
+    // Getters
+    public RulesPackage Get_Rules(){return _rules;}
+
+    public GameBoard Get_GameBoard() {return _gameBoard;}
+
+    public Player Get_CurrentPlayer() {return _currentPlayer;}
+
+    public int Get_CurrentDay(){return _currentDay;}
+
+    // Setters
+
+    public void Set_Rules(RulesPackage rules){this._rules = rules;}
+
+    public void Set_GameBoard(GameBoard _gameBoard) {this._gameBoard = _gameBoard;}
+
+    public void Set_CurrentPlayer(Player _currentPlayer) {this._currentPlayer = _currentPlayer;}
+
+    public void Set_CurrentDay(int day){this._currentDay = day;}
+
     /**
      * @param playerAction any player action that implements the TurnAction interface
      */
     private void SetPlayerAction(TurnAction playerAction) {
         _playerAction = playerAction;
+    }
+
+
+    /**
+     * This makes sure that the player is rewarded after their turn and calls the
+     * function that deals with ending the day/game.
+     */
+    private void UpdateGame()
+    //This plays after the player is done with a turn, rewarding the player and checking if the day/game is over.
+    {
+        PlayerManager manager = PlayerManager.GetInstance();
+        Player current = Get_CurrentPlayer();
+        LocationComponent loc = current.Get_Location();
+        // Check if scene just wrapped
+        if (loc.Get_CurrentGameSet() instanceof ActingSet actSet)
+        {
+            if (actSet.IsComplete())
+            {
+                manager.BonusPay(Get_CurrentPlayer());
+                actSet.RemoveCard();
+            }
+        }
+
+        // Check if day is over
+        if (IsEndDay())
+        {
+            EndDay();
+            return;
+        }
+        // Move to next player
+        AdvanceTurn();
+    }
+
+    /**
+     * Gets called if there is only one scene card remaining UpdateGame.
+     * Removes SceneCards and Resets Shot Tokens
+     */
+    private void EndDay() //may be off by 1
+    {
+        PlayerManager manager = PlayerManager.GetInstance();
+        Set_CurrentDay(Get_CurrentDay() + 1);
+
+        if (IsEndGame()) {
+            EndGame();
+            return;
+        }
+
+        GameSet trailer = Get_GameBoard().Get_StartingSet();
+        for (Player p : manager.Get_PlayerLibrary())
+        {
+            LocationComponent loc = p.Get_Location();
+
+            // remove from old set
+            GameSet oldSet = loc.Get_CurrentGameSet();
+            if (oldSet != null) {
+                oldSet.RemovePlayer(p);
+            }
+
+            // reset player state
+            loc.Set_CurrentRole(null);
+            loc.Set_OnCard(false);
+            loc.Set_RehearseTokens(0);
+
+            // move to trailer
+            loc.Set_CurrentGameSet(trailer);
+            trailer.AddPlayer(p);
+        }
+
+        Get_GameBoard().Clear();
+        Get_GameBoard().Populate();
+    }
+
+
+    /**
+     * This Function is a Helper for UpdateGame that moves focus to the next player
+     * after the game is done updating.
+     */
+    private void AdvanceTurn()
+    {
+        PlayerManager manager = PlayerManager.GetInstance();
+        int index = 0;
+
+        for (int i = 0; i < manager.Get_PlayerLibrary().length; i++)
+        {
+            if (manager.Get_PlayerLibrary()[i] == _currentPlayer)
+            {
+                index = i;
+                break;
+            }
+        }
+        _currentPlayer = manager.Get_PlayerLibrary()[(index + 1) % manager.Get_PlayerLibrary().length];
+    }
+
+    /**
+     * Checks the ammount of scene cards in play to know how many remain
+     * If there is one left returns True
+     * @return boolean
+     */
+    public boolean IsEndDay()
+    {
+        int activeScenes = 0;
+
+        for (GameSet set : _gameBoard.GetAllGameSets()) {
+            if (set instanceof ActingSet act) {
+                if (act.Get_CurrentSceneCard() != null) {
+                    activeScenes++;
+                }
+            }
+        }
+        return activeScenes <= 1;
+    }
+
+    /**
+     * compares day limit to current day, if they  are the same return true
+     * @return boolean
+     */
+    public boolean IsEndGame() {return Get_Rules().GetDays() == Get_CurrentDay();}
+
+
+    /**
+     * This function ends the game (:  It grabs the score of each player and gets the winners, next it displays the
+     * winners to the players.
+     */
+    public void EndGame()
+    {
+        PlayerManager manager = PlayerManager.GetInstance();
+        int[] scores = manager.TallyScore();
+
+        // Grab the highest score
+        int highest = Integer.MIN_VALUE;
+        for (int score : scores) {
+            if (score > highest) {
+                highest = score;
+            }
+        }
+
+        // Grab Winner
+        ArrayList<Player> winners = new ArrayList<>();//Could add a tiebreaker piece.
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] == highest) {
+                winners.add(manager.Get_PlayerLibrary()[i]);
+            }
+        }
+
+        // Display
+        System.out.println("=== GAME OVER ===");
+        for (Player winner : winners)
+        {
+            System.out.println("Winner: Player " + winner.Get_PersonalId() + " with score " + winner.Get_Score());
+        }
+    }
+
+    /**
+     *This starts the game using the given rules package as well as sets the day to one and moves players to the start.
+     * Next it sets up all the players default values then populates the board and chooses a starting plyer.
+     */
+    public void StartGame()
+    {
+        PlayerManager manager = PlayerManager.GetInstance();
+        UpdateRules();
+
+        Set_CurrentDay(1);
+
+        // Move all players to Trailer
+        GameSet trailer = _gameBoard.Get_StartingSet();
+
+        for (Player p : manager.Get_PlayerLibrary())
+        {
+            LocationComponent loc = p.Get_Location();
+
+            loc.Set_CurrentRole(null);
+            loc.Set_OnCard(false);
+            loc.Set_RehearseTokens(0);
+
+            loc.Set_CurrentGameSet(trailer);
+            trailer.AddPlayer(p);
+        }
+
+        // Populate board with scene cards
+        _gameBoard.Populate();
+
+        // Choose first player
+        _currentPlayer = manager.Get_PlayerLibrary()[0]; //Could make this a dice roll.
+    }
+
+    private void UpdateRules()
+    //This is used after Deadwood asks for players to change the rules depending on the player number.
+    {
+        PlayerManager manager = PlayerManager.GetInstance();
+        RulesPackage rules = new RulesPackage(manager.Get_PlayerLibrary());
+
+        // store for later
+        _rules = rules;
+
+        // apply to players
+        for (Player p : manager.Get_PlayerLibrary()) {
+            p.Set_CurrentRank(rules.GetStartingRank());
+            p.Get_Currency().IncreaseCredits(rules.GetStartingCredits());
+        }
     }
 
 
--- src/PlayerManager.java
+++ src/PlayerManager.java
@@ -1,221 +1,54 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class PlayerManager {
 
     // Members
+    private static PlayerManager _instance;
     private GameBoard _gameBoard;
     private Player[] _playerLibrary;
-    private Player _currentPlayer;
-    private int _currentDay;
-    private RulesPackage _rules;
-
 
     // Constructors
-    public PlayerManager(){
-    }
+    private PlayerManager() {}
 
-    public PlayerManager(GameBoard gameBoard)
-    {
-        this._gameBoard = gameBoard;
-    }
-
-    public PlayerManager(GameBoard gameBoard, Player[] players)
-    {
-        this._gameBoard = gameBoard;
-        this._playerLibrary = players;
-    }
-
-    public PlayerManager(GameBoard gameBoard, Player[] players, Player currentPlayer)
-    {
-        this._gameBoard = gameBoard;
-        this._playerLibrary = players;
-        this._currentPlayer = currentPlayer;
-    }
-
-    public PlayerManager(GameBoard gameBoard, Player[] players, Player currentPlayer, RulesPackage rules)
-    {
-        this._gameBoard = gameBoard;
-        this._playerLibrary = players;
-        this._currentPlayer = currentPlayer;
-        this._rules = rules;
+    public static PlayerManager GetInstance() { //How does this work with setting the players and all that
+        if (_instance == null)
+        {
+            _instance = new PlayerManager();
+        }
+        return _instance;
     }
 
     // Getters
-
-    public Player Get_CurrentPlayer() {return _currentPlayer;}
-
     public GameBoard Get_GameBoard() {return _gameBoard;}
 
     public Player[] Get_PlayerLibrary() {return _playerLibrary;}
-
-    public int Get_CurrentDay(){return _currentDay;}
-
-    public RulesPackage Get_Rules(){return _rules;}
 
     public int Get_PlayerCount() {
         return _playerLibrary == null ? 0 : _playerLibrary.length;
     }
 
     // Setters
-
-    public void Set_CurrentPlayer(Player _currentPlayer) {this._currentPlayer = _currentPlayer;}
-
     public void Set_GameBoard(GameBoard _gameBoard) {this._gameBoard = _gameBoard;}
 
     public void Set_PlayerLibrary(Player[] _playerLibrary) {this._playerLibrary = _playerLibrary;}
 
-    public void Set_CurrentDay(int day){this._currentDay = day;}
-
-    public void Set_Rules(RulesPackage rules){this._rules = rules;}
 
     // Methods
-    private void UpdateRules()
-    //This is used after Deadwood asks for players to change the rules depending on the player number.
-    {
-        RulesPackage rules = new RulesPackage(_playerLibrary);
-
-        // store for later
-        _rules = rules;
-
-        // apply to players
-        for (Player p : _playerLibrary) {
-            p.Set_CurrentRank(rules.GetStartingRank());
-            p.Get_Currency().IncreaseCredits(rules.GetStartingCredits());
-        }
-    }
-
-    /**
-     * This makes sure that the player is rewarded after their turn and calls the
-     * function that deals with ending the day/game.
-     */
-    private void UpdateGame()
-    //This plays after the player is done with a turn, rewarding the player and checking if the day/game is over.
-    {
-        Player current = Get_CurrentPlayer();
-        LocationComponent loc = current.Get_Location();
-
-        // Check if scene just wrapped
-        if (loc.Get_CurrentGameSet() instanceof ActingSet actSet)
-        {
-            if (actSet.IsComplete())
-            {
-                BonusPay();
-                actSet.RemoveCard();
-            }
-        }
-
-        // Check if day is over
-        if (IsEndDay())
-        {
-            EndDay();
-            return;
-        }
-        // Move to next player
-        AdvanceTurn();
-    }
-
-    /**
-     * This Function is a Helper for UpdateGame that moves focus to the next player
-     * after the game is done updating.
-     */
-    private void AdvanceTurn()
-    {
-        int index = 0;
-
-        for (int i = 0; i < _playerLibrary.length; i++)
-        {
-            if (_playerLibrary[i] == _currentPlayer)
-            {
-                index = i;
-                break;
-            }
-        }
-
-        _currentPlayer = _playerLibrary[(index + 1) % _playerLibrary.length];
-    }
-
-    /**
-     * Gets called if there is only one scene card remaining UpdateGame.
-     * Removes SceneCards and Resets Shot Tokens
-     */
-    private void EndDay()
-    {
-        Set_CurrentDay(Get_CurrentDay() + 1);
-
-        if (IsEndGame()) {
-            EndGame();
-            return;
-        }
-
-        GameSet trailer = Get_GameBoard().Get_StartingSet();
-        for (Player p : _playerLibrary)
-        {
-            LocationComponent loc = p.Get_Location();
-
-            // remove from old set
-            GameSet oldSet = loc.Get_CurrentGameSet();
-            if (oldSet != null) {
-                oldSet.RemovePlayer(p);
-            }
-
-            // reset player state
-            loc.Set_CurrentRole(null);
-            loc.Set_OnCard(false);
-            loc.Set_RehearseTokens(0);
-
-            // move to trailer
-            loc.Set_CurrentGameSet(trailer);
-            trailer.AddPlayer(p);
-        }
-
-        Get_GameBoard().Clear();
-        Get_GameBoard().Populate();
-    }
-
-
-    /**
-     * Checks the ammount of scene cards in play to know how many remain
-     * If there is one left returns True
-     * @return boolean
-     */
-    public boolean IsEndDay()
-    {
-        int activeScenes = 0;
-
-        for (GameSet set : _gameBoard.GetAllGameSets()) {
-            if (set instanceof ActingSet act) {
-                if (act.Get_CurrentSceneCard() != null) {
-                    activeScenes++;
-                }
-            }
-        }
-        return activeScenes <= 1;
-    }
-
-    /**
-     * compares day limit to current day, if they  are the same return true
-     * @return boolean
-     */
-    public boolean IsEndGame()
-    {
-        return Get_Rules().GetDays() == Get_CurrentDay();
-    }
-
     /**
      * This looks at if the roll was a success and pays the players
      * My problem is where is this called?
      * If its in update then this WILL NOT WORK.
      */
-    public void BasicPay(boolean success)
+    public void BasicPay(Player player, boolean success)
     {
         //I need to make a way to exclude people who are just hanging out on the card!
-        Player player = Get_CurrentPlayer();
         if(player.Get_Location().Get_OnCard())
         {
-                if(success) //if the roll is a success
-                {
-                    player.Get_Currency().IncreaseCoins(2);
-                }
+            if(success) //if the roll is a success
+            {
+                player.Get_Currency().IncreaseCoins(2);
+            }
         }
         else
         {
@@ -237,10 +70,10 @@ public class PlayerManager {
      * card members assigning them the previously rolled money dice starting with the player on the highest rank
      * and descending in order.  Next it pays off card members based on rank.
      */
-    public void BonusPay()
+    public void BonusPay(Player currentPlayer)
     {
-        Player[] players = Get_PlayerLibrary();
-        Player currentPlayer = Get_CurrentPlayer();
+        Player[] players = Get_PlayerLibrary();;
+        if (players == null || players.length == 0) return;
         ArrayList<Player> onCard = new ArrayList<>();
         ArrayList<Player> offCard = new ArrayList<>();
         GameSet currentSet = currentPlayer.Get_Location().Get_CurrentGameSet();
@@ -258,7 +91,7 @@ public class PlayerManager {
         for (int i = 0; i < difficulty; i++) {
             rolls.add(dice.Roll(1, 6)); // 1d6
         }
-        rolls.sort((a, b) -> b - a); // descending
+        rolls.sort(Comparator.reverseOrder());// descending
 
         // Sort Players
         for(Player player : players){
@@ -301,12 +134,16 @@ public class PlayerManager {
 
     /**
      * Runs through the list of players and Tallys their Score
-     * @param players
      * @return int[] of player scores
      */
-    public int[] TallyScore(Player[] players)
+    public int[] TallyScore()
     //as the last day finishes this tallies the score and displays it before the Deadwood ends.
     {
+        Player[] players = Get_PlayerLibrary();
+        if (players == null || players.length == 0)
+            {
+                return new int[0];
+            }
         int[] scores = new int[players.length];
 
         //for each player calculate their score then retrieve it.
@@ -319,82 +156,18 @@ public class PlayerManager {
         return scores;
     }
 
-    /**
-     *This starts the game using the given rules package as well as sets the day to one and moves players to the start.
-     * Next it sets up all the players default values then populates the board and chooses a starting plyer.
-     */
-    public void StartGame()
-    {
-        UpdateRules();
-
-        Set_CurrentDay(1);
-
-        // Move all players to Trailer
-        GameSet trailer = _gameBoard.Get_StartingSet();
-
-        for (Player p : _playerLibrary)
-        {
-            LocationComponent loc = p.Get_Location();
-
-            loc.Set_CurrentRole(null);
-            loc.Set_OnCard(false);
-            loc.Set_RehearseTokens(0);
-
-            loc.Set_CurrentGameSet(trailer);
-            trailer.AddPlayer(p);
-        }
-
-        // Populate board with scene cards
-        _gameBoard.Populate();
-
-        // Choose first player
-        _currentPlayer = _playerLibrary[0]; //Could make this a dice roll.
-    }
-
-    /**
-     * This function ends the game (:  It grabs the score of each player and gets the winners, next it displays the
-     * winners to the players.
-     */
-    public void EndGame()
-    {
-        int[] scores = TallyScore(_playerLibrary);
-
-        // Grab the highest score
-        int highest = Integer.MIN_VALUE;
-        for (int score : scores) {
-            if (score > highest) {
-                highest = score;
-            }
-        }
-
-        // Grab Winner
-        ArrayList<Player> winners = new ArrayList<>();//Could add a tiebreaker piece.
-        for (int i = 0; i < scores.length; i++) {
-            if (scores[i] == highest) {
-                winners.add(_playerLibrary[i]);
-            }
-        }
-        
-        // Display
-        System.out.println("=== GAME OVER ===");
-        for (Player winner : winners)
-        {
-            System.out.println("Winner: Player " + winner.Get_PersonalId() + " with score " + winner.Get_Score());
-        }
-    }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("GameManager {\n");
+        sb.append("PlayerManager {\n");
 
         sb.append("  GameBoard: ");
         sb.append(_gameBoard != null ? _gameBoard.toString() : "null");
         sb.append("\n");
 
         sb.append("  Current Player: ");
-        sb.append(_currentPlayer != null ? _currentPlayer.toString() : "null");
         sb.append("\n");
 
         sb.append("  Player Count: ");
