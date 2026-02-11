2d97912ce7cc29e166160323ab9a088885958491
Jonathan Marien <90732393+Mightysmallfry@users.noreply.github.com>
2026-02-10 17:01:17 -0800

    Move Action Implementaton

--- src/Move.java
+++ src/Move.java
@@ -11,17 +11,17 @@ public class Move implements TurnAction{
         // Get the player's current location
 
         Player currentPlayer = GameManager.GetInstance().GetCurrentPlayer();
-        GameSet currentLocation = currentPlayer.Get_Location().Get_CurrentGameSet();
+        GameSet currentSet = currentPlayer.Get_Location().Get_CurrentGameSet();
 
         // Get the neighbors of current location
-        HashMap<String, GameSet> neighbors = currentLocation.GetNeighbors();
+        HashMap<String, GameSet> neighbors = currentSet.GetNeighbors();
 
         // Get the player's input
         while (!_valid)
         {
             // Check validity
             String playerInput = Input.nextLine().toLowerCase().strip();
-            if (playerInput.equals(currentLocation.GetName())){
+            if (playerInput.equals(currentSet.GetName())){
                 System.out.println("You're already there!");
                 return;
             }
@@ -37,11 +37,18 @@ public class Move implements TurnAction{
                 _valid = !_valid;
             }
         }
-        
 
         // Update player location
-        //  Accounting for both locationComponent and both GameSets involved
+
+        // Update new Location
+        GameSet targetLocation = currentSet.GetNeighbors().get(playerInput);
+        targetLocation.AddPlayer(currentPlayer);
+
+        // Update old Location and Player
+        currentSet.GetPlayers().remove(currentPlayer);
+        currentPlayer.Get_Location().Set_CurrentGameSet(targetLocation);
 
         // return to action select
+        // aka just finish executing
     }
 }
