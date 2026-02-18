7651e2e50a77af3b78d1fc22a527c47570b92f34
Gus <wischma@wwu.edu>
2026-02-17 21:52:12 -0800

    forgot to add this one

---
+++ src/mothman/utils/TurnDisplayInfo.java
@@ -0,0 +1,13 @@
+package mothman.utils;
+
+public class TurnDisplayInfo {
+    public String playerId;
+    public String locationName;
+    public String roleLine;
+    public int budget;
+    public int maxShots;
+    public int currentShots;
+    public int actionTokens;
+    public boolean sceneComplete;
+    public boolean isActingSet;
+}
